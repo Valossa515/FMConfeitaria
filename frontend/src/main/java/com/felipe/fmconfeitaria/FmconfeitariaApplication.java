d48e908c15fc39e6e7c888c69ce945ae2d330335
Valossa515 <fe.mmo515@gmail.com>
2023-08-10 14:20:16 -0300

    primeira classe de service e controller

--- frontend/src/main/java/com/felipe/fmconfeitaria/FmconfeitariaApplication.java
+++ frontend/src/main/java/com/felipe/fmconfeitaria/FmconfeitariaApplication.java
@@ -1,13 +1,18 @@
 package com.felipe.fmconfeitaria;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FmconfeitariaApplication {
+public class FmconfeitariaApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FmconfeitariaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
---
+++ frontend/src/main/java/com/felipe/fmconfeitaria/services/CategoriaService.java
@@ -0,0 +1,71 @@
+package com.felipe.fmconfeitaria.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.felipe.fmconfeitaria.dto.CategoriaDTO;
+import com.felipe.fmconfeitaria.entities.Categoria;
+import com.felipe.fmconfeitaria.repositories.CategoriaRepository;
+import com.felipe.fmconfeitaria.services.exceptions.DataIntegrityException;
+import com.felipe.fmconfeitaria.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Categoria find(Long id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName() ));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return categoriaRepository.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return categoriaRepository.save(newObj);
+	}
+	
+	public void delete(Long id) {
+		find(id);
+		
+		try {
+			categoriaRepository.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma cateogria que possui produtos!");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, 
+			String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, 
+				Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	public void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
+	
+}
